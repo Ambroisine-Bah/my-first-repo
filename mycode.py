7fdb781f88bf9ebfde7cd26e8c5e646ff4090cd1
Ambroisine-Bah <abah035@uottawa.ca>
2025-04-02 20:20:33 -0400

    added substancial changes to mycode that creates a list of feature classes in a geodatabase

--- mycode.py
+++ mycode.py
@@ -1,4 +1,6 @@
 import arcpy
 from arcpy import env
-env.workspace=r"C:\Users\abah035\Desktop\Lecture 7\Module7_Data\OTTAWA.gdb\Ottawadata"
-print(arcpy.ListFeatureClasses())
+env.workspace=r"C:\Users\abah035\Desktop\Lecture 7\Module7_Data\OTTAWA.gdb\OTTAWADATA"
+featuresclass_list=(arcpy.ListFeatureClasses()
+for fc in featuresclass_list=
+    print (my_function_module.get_number_fields(fc))
